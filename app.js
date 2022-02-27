e5bb33bc52851f89f5e52c1ba2a5db39ac6fe96d
DebdutBiswas <rupam.biswas@outlook.com>
2022-02-27 11:14:15 +0530

    Added Middlewares and Routes

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const db = require('./configs/database');
 
 process.env.NODE_ENV = process.env.NODE_ENV ?? 'development';
+
+let authMiddleware = require('./middlewares/auth');
+
+const authRouter = require('./routes/auth');
+const usersRouter = require('./routes/users');
+const postsRouter = require('./routes/posts');
+
+db.authenticate().then(() => {
+    console.log('Database connected...');
+    // Create database tables if not exists
+    // (async () => await db.sync())();
+}).catch(err => {
+    console.log('Error: ' + err);
+    process.exit();
+});
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -30,6 +46,19 @@ app.use((err, req, res, next) => {
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to CMS API"});
 });
+
+// Auth route
+app.use('/auth', authRouter);
+
+// Auth middlewares
+// app.use(authMiddleware);
+
+// Bypass auth middlewares (debug)
+process.env.NODE_ENV === 'debug' ? authMiddleware = (req, res, next) => { next() } : console.log({ NODE_ENV: process.env.NODE_ENV });
+
+// App routes
+app.use('/users', authMiddleware, usersRouter);
+app.use('/posts', authMiddleware, postsRouter);
 
 // Invalid routes
 app.use('/*', (req, res)=>{
